dfab76b10ce43db2235e58eba5c570d724e76f49
NaveenPrabakar <123602587+NaveenPrabakar@users.noreply.github.com>
2024-07-21 16:13:10 -0500

    Update QuizTables.sql

--- QuizTables.sql
+++ QuizTables.sql
@@ -1,3 +1,19 @@
 use discordbot;
 
 -- Creating the tables for the question bank
+Create Table cote(
+question varchar(200),
+answer1 varchar(200),
+answer2 varchar(200),
+answer3 varchar(200),
+correct varchar(200),
+primary key(question)
+);
+
+-- Stores picture locations of the anime
+Create Table pictures(
+anime varchar(200),
+picture varchar(200),
+
+primary key(picture)
+);
