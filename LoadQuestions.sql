e54ac326c9d709cdcddbd30219e279c16175dd42
NaveenPrabakar <123602587+NaveenPrabakar@users.noreply.github.com>
2024-07-21 16:13:35 -0500

    Update LoadQuestions.sql

--- LoadQuestions.sql
+++ LoadQuestions.sql
@@ -3,9 +3,13 @@ use discordbot;
 Select @@secure_file_priv; -- put all csv files through this directory
 
 -- change directory to whatever you need it to be
-Load data infile 'C:\\ProgramData\\MySQL\\MySQL Server 8.3\\Uploads\\'
+Load data infile 'C:\\ProgramData\\MySQL\\MySQL Server 8.3\\Uploads\\Book1.csv'
 into table cote
 fields terminated by ','
 enclosed by '"'
-Lines terminated by '\n'
+Lines terminated by '\n';
+
+-- Inserts picture locations to table
+Insert into pictures(anime, picture)
+Value('cote', 'cote.png');
 
