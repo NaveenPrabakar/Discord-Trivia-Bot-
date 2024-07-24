e1264419a72fbad42eb5a7e681acf28caa00554c
NaveenPrabakar <123602587+NaveenPrabakar@users.noreply.github.com>
2024-07-24 17:17:01 -0500

    Update LoadQuestions.sql

--- LoadQuestions.sql
+++ LoadQuestions.sql
@@ -9,7 +9,68 @@ fields terminated by ','
 enclosed by '"'
 Lines terminated by '\n';
 
+Load data infile 'C:\\ProgramData\\MySQL\\MySQL Server 8.3\\Uploads\\blackClover.csv'
+into table blackclover
+fields terminated by ','
+enclosed by '"'
+Lines terminated by '\n';
+
+Load data infile 'C:\\ProgramData\\MySQL\\MySQL Server 8.3\\Uploads\\naruto.csv'
+into table naruto
+fields terminated by ','
+enclosed by '"'
+Lines terminated by '\n';
+
+Load data infile 'C:\\ProgramData\\MySQL\\MySQL Server 8.3\\Uploads\\genshin.csv'
+into table genshin
+fields terminated by ','
+enclosed by '"'
+Lines terminated by '\n';
+
+Load data infile 'C:\\ProgramData\\MySQL\\MySQL Server 8.3\\Uploads\\bleach.csv'
+into table bleach
+fields terminated by ','
+enclosed by '"'
+Lines terminated by '\n';
+
+Load data infile 'C:\\ProgramData\\MySQL\\MySQL Server 8.3\\Uploads\\onepiece.csv'
+into table onepiece
+fields terminated by ','
+enclosed by '"'
+Lines terminated by '\n';
+
+Load data infile 'C:\\ProgramData\\MySQL\\MySQL Server 8.3\\Uploads\\roshidere.csv'
+into table roshidere
+fields terminated by ','
+enclosed by '"'
+Lines terminated by '\n';
+
+Load data infile 'C:\\ProgramData\\MySQL\\MySQL Server 8.3\\Uploads\\toradora.csv'
+into table toradora
+fields terminated by ','
+enclosed by '"'
+Lines terminated by '\n';
+
+Load data infile 'C:\\ProgramData\\MySQL\\MySQL Server 8.3\\Uploads\\intialD.csv'
+into table intiald
+fields terminated by ','
+enclosed by '"'
+Lines terminated by '\n';
+
+Load data infile 'C:\\ProgramData\\MySQL\\MySQL Server 8.3\\Uploads\\eminanceInshadow.csv' ignore
+into table eminanceinshadow
+fields terminated by ','
+enclosed by '"'
+Lines terminated by '\n';
+
+
+
+
+
 -- Inserts picture locations to table
 Insert into pictures(anime, picture)
 Value('cote', 'cote.png');
+
+
+
 
